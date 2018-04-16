769cb4f52b336cef4ada5d5f9d34b40e88b1b6f6
jlhelwig <jeff.helwig@gmail.com>
2018-04-16 14:23:10 -0500

    update

--- test/appTest.js
+++ test/appTest.js
@@ -1,23 +1,23 @@
 const assert = require('chai').assert;
-const sayHello = require('../script').sayHello;
-const addNumbers =  require('../script').addNumbers;
-// const app = require ('../script');
+// const sayHello = require('../script').sayHello;
+// const addNumbers =  require('../script').addNumbers;
+const app = require ('../script');
 
 describe ('App',function(){
   it ('should equal hello', function(){
-    let result = sayHello()
+    let result = app.sayHello()
     assert.equal(result,'hello');
   });
   it ('sayHello should be a string', function(){
-    let result = sayHello()
+    let result = app.sayHello()
     assert.typeOf(result, 'string');
   });
   it ('addNumbers should be greater than 5', function(){
-    let result =addNumbers(3,3)
+    let result =app.addNumbers(3,3)
     assert.isAbove(result,5);
   });
   it ('addNumbers should be a number', function(){
-    let result = addNumbers(3,3)
+    let result = app.addNumbers(3,3)
     assert.typeOf(result, 'number');
   });
 });
